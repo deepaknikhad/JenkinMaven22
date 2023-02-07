f4a552cd007f5969b04e2610efd7571aa2de3e99
Ghost <deepaknikhad>
2023-02-08 00:02:39 +0530

    parameter added

--- src/test/java/mavenForJenkins/UITest.java
+++ src/test/java/mavenForJenkins/UITest.java
@@ -1,27 +1,45 @@
 package mavenForJenkins;
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class UITest 
 {
 	
+	@Parameters("Browser")
 	@Test
-	public void startBrowser()
+	public void startBrowser(String browserName)
 	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		System.out.println("Parameter value is " + browserName );
+		WebDriver driver = null;
+		
+		if(browserName.contains("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		
+		else if(browserName.contains("Edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		
+		else if(browserName.contains("Gecko"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.get("https://www.instagram.com/");
+		Assert.assertTrue(driver.getTitle().contains("Instagram"), "Title does not match");		
 		driver.quit();
 		
 	}
